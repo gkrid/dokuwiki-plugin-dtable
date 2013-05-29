e73e02fcb4b88d34be18c35fa257d75f9ce0c14f
Szymon Olewniczak <szymon.olewniczak@rid.pl>
2013-05-29 09:16:51 +0200

    config changes

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,4 +1,5 @@
 <?php
 
-$lang['bases_dir'] = 'Direcotry where the bases will be storage. You can give absolute or relative path(relation to main dokuwiki dir).';
 $lang['max_table_width'] = 'Table width in pixels of form row. Decrease it when your form row is too width that it doesn\'t fit your template.';
+$lang['all_tables'] = 'Transform all dokuwiki tables to dtables';
+$lang['all_exttabs'] = 'Transform all exttabs to dtables';
--- lang/pl/settings.php
+++ lang/pl/settings.php
@@ -1,4 +1,5 @@
 <?php
 
-$lang['bases_dir'] = 'Katalog w którym będą przechowywane bazy danych. Możesz podać ścierzkę bezwzględną lub względną (względem głównego katalogu wiki).';
 $lang['max_table_width'] = 'Szerokość wiersza z formularzem podawana w pikselach. Pomniejsz tą wartość jeżeli wiersz formularza jest zbyt szeroki dla twojego szablonu.';
+$lang['all_tables'] = 'Przekształć wszystkie tabele wiki na dtabele.';
+$lang['all_exttabs'] = 'Przekształć wszystkie exttabele na dtabele.';
